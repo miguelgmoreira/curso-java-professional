c6992bd92783feea5cf9b01695054432f584a907
Miguel Garcia <miguelfiuza315@gmail.com>
2024-08-27 15:48:36 -0300

    refact: estrutura pastas

---
+++ app-calculo/src/br/com/cod3r/app/calculo/Calculadora.java
@@ -0,0 +1,15 @@
+package br.com.cod3r.app.calculo;
+
+import br.com.cod3r.app.calculo.interno.OperacoesAritmeticas;
+import br.com.cod3r.app.logging.Logger;
+
+
+public class Calculadora {
+
+   private OperacoesAritmeticas opAtirmeticas = new OperacoesAritmeticas();
+
+    public double soma(double... nums) {
+        Logger.info("Somando...");
+        return opAtirmeticas.soma(nums);
+    }
+}
